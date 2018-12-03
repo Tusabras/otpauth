8c002e1d5d32bbd9a0c4d16672455c59269e0275
Albert Camps Oller <campsupc@gmail.com>
2018-12-03 10:02:26 +0100

    second commit

--- src/controllers/home.ts
+++ src/controllers/home.ts
@@ -2,6 +2,7 @@ import { Request, Response, NextFunction } from "express";
 const speakeasy = require('speakeasy');
 const QRCode = require('qrcode');
 let secret = '';
+
 /**
  * GET /
  * Home page.
@@ -9,8 +10,9 @@ let secret = '';
 export let index = (req: Request, res: Response) => {
 
   const { base32, otpauth_url } = speakeasy.generateSecret({length: 20});
-  // console.log(base32, otpauth_url); // Save this value to your DB for the user
-  // Example: 
+
+  // Deberiamos guardarnos el valor del base32 en la BD de cada usuario.
+  // Ejemplo: 
   // base32: MMST4KKLFBCDQL2HEFXDKLSEIJ2TEJBP 
   // otpauth_url: otpauth://totp/SecretKey?secret=MMST4KKLFBCDQL2HEFXDKLSEIJ2TEJBP
 
@@ -31,10 +33,9 @@ export let index = (req: Request, res: Response) => {
  */
 export let postOTP = (req: Request, res: Response, next: NextFunction) => {
   console.log(req.body);
-  // This is provided the by the user via form POST
   const userToken = req.body.otp;
 
-  // Verify that the user token matches what it should at this moment
+  // Verificamos que el token del usuario hace match.
   const verified = speakeasy.totp.verify({
     secret: secret,
     encoding: 'base32',
@@ -43,25 +44,4 @@ export let postOTP = (req: Request, res: Response, next: NextFunction) => {
   if (!verified) req.flash("errors", { msg: "El código proporcionado es incorrecto!" });
   else req.flash("success", { msg: "El código proporcionado es correcto!" });
   res.redirect(req.session.returnTo || "/");
-  // req.assert("otp", "El codigo no puede estar vacio").notEmpty();
-
-  // const errors = req.validationErrors();
-
-  // if (errors) {
-  //   req.flash("errors", errors);
-  //   return res.redirect("/login");
-  // }
-
-  // passport.authenticate("local", (err: Error, user: UserModel, info: IVerifyOptions) => {
-  //   if (err) { return next(err); }
-  //   if (!user) {
-  //     req.flash("errors", info.message);
-  //     return res.redirect("/login");
-  //   }
-  //   req.logIn(user, (err) => {
-      // if (err) { return next(err); }
-      // req.flash("success", { msg: "Success! You are logged in." });
-      // res.redirect(req.session.returnTo || "/");
-  //   });
-  // })(req, res, next);
 };
